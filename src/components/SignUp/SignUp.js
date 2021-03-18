89433c663e2fd47f8f7c3d64fb01eb180b264959
Hermann Marx <marxhermann@web.de>
2021-03-18 18:22:34 +0100

    asdf

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+/* import "./styles.css";
 import { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
@@ -197,3 +197,32 @@ const SignUp = ({ userUpdate }) => {
 };
 
 export default SignUp;
+ */
+
+import axios from "axios";
+
+function App() {
+  const submit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://matchup-be.herokuapp.com/users/login", {
+        username: "Hermann",
+        password: "123",
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => console.log("error", e));
+  };
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
